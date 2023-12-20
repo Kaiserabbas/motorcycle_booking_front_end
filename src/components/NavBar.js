fd4ac954f14e03623f5a8d2bfc4cf016257e2416
unknown <zelalemyohannes75@gmail.com>
2023-12-20 16:13:36 +0300

    add styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,44 @@
-import React from "react";
-import './NavBar.css';
+import React, { useState } from "react";
+import "./NavBar.css";
 
 const NavBar = () => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible);
+  };
+
   return (
-    <div>
+    <div className="nav-container">
+      <div className={`navigation ${isVisible ? "" : "hidden"}`}>
         <h1>TITLE</h1>
         <ul className="nav-list">
           <li>MODELS</li>
-
           <li>LIFESTYLE</li>
-
           <li>SHOP</li>
-
           <li>TEST DRIVE</li>
         </ul>
 
-        <ul className="icons"> 
-            <li><i className="fab fa-google"></i></li>
-            <li><i className="fab fa-twitter"></i></li>
-            <li><i className="fab fa-facebook"></i></li>
-            <li><i className="fab fa-pinterest"></i></li>
-
+        <ul className="icons">
+          <li>
+            <i className="fab fa-google"></i>
+          </li>
+          <li>
+            <i className="fab fa-twitter"></i>
+          </li>
+          <li>
+            <i className="fab fa-facebook"></i>
+          </li>
+          <li>
+            <i className="fab fa-pinterest"></i>
+          </li>
         </ul>
+      </div>
+      <div className="toggler" onClick={toggleVisibility}>
+        <i class="fa fa-caret-left" aria-hidden="true"></i>
+      </div>
     </div>
   );
 };
+
 export default NavBar;
