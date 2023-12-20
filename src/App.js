8f47b2b09c26d371a8ad7f31042b127b29868368
unknown <zelalemyohannes75@gmail.com>
2023-12-20 15:38:26 +0300

    add icons

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from "./components/NavBar";
 import Main from "./components/Main";
 import Details from "./components/Details";
 import './App.css'
+import '@fortawesome/fontawesome-free/css/all.min.css';
 const App =()=>{
   return(
     <div className="main-ui">
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,28 @@
 import React from "react";
+import './NavBar.css';
 
-const NavBar =() => {
-    return(
-        <div>
-            <h1>
-                This is the NavBar
-            </h1>
-        </div>
-    )
-}
+const NavBar = () => {
+  return (
+    <div>
+        <h1>TITLE</h1>
+        <ul className="nav-list">
+          <li>MODELS</li>
+
+          <li>LIFESTYLE</li>
+
+          <li>SHOP</li>
+
+          <li>TEST DRIVE</li>
+        </ul>
+
+        <ul className="icons"> 
+            <li><i className="fab fa-google"></i></li>
+            <li><i className="fab fa-twitter"></i></li>
+            <li><i className="fab fa-facebook"></i></li>
+            <li><i className="fab fa-pinterest"></i></li>
+
+        </ul>
+    </div>
+  );
+};
 export default NavBar;
