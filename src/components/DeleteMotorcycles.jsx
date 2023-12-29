cf33d6aab83c961191d32b55c790c4bc4db21974
albertkantwi <albertkantwi@gmail.com>
2023-12-30 00:16:37 +1400

    add the DeleteItem component and its functionality

--- src/components/DeleteMotorcycles.jsx
+++ src/components/DeleteMotorcycles.jsx
@@ -1,12 +1,32 @@
 import { Navigate } from "react-router-dom";
-
+import '../style/deleteMotorcycle.css';
+import DeleteItem from "./DeteteItem";
+import DeleteModal from "./Modal";
+import NavBar from "./NavBar";
 
 const DeleteMotorcycles= ()=>{
     if(!JSON.parse(localStorage.getItem('session_token'))?.token)return(<Navigate to="/"/>);
+    
+
     return(
-        <h1>
-            I will delete all the motorcycles!
-        </h1>
+        <>
+        <DeleteModal/>
+        <section className="mainUi">
+            <NavBar/>
+            <div>
+            <header className="mainBodyHeader flexV">
+        <h1>MOTORCYCLES LIST</h1>
+        <hr className='bar' />
+            </header>
+            <DeleteItem/>
+            <DeleteItem/>
+            <DeleteItem/>
+            <DeleteItem/>
+            <DeleteItem/>
+            </div>
+        </section>
+            
+        </>
     );
 };
 
