e76d2f05f8b3bdf0d6ded96cca4f74df543a4239
Graciano1997 <gracianomanuelhenrique@gmail.com>
2024-01-02 14:13:36 +0100

    add the url entr7y point

--- src/urls.js
+++ src/urls.js
@@ -1,13 +1,21 @@
 let tokenObject=undefined
 let request_header=undefined;
+let currentUser=undefined
 
 if(localStorage.getItem('session_token')){
     tokenObject = JSON.parse(localStorage.getItem('session_token'));
     request_header= {headers: {'Authorization': `Bearer ${tokenObject.token}`,'Content-Type': 'application/json'}}
+    currentUser=tokenObject.user.name;
 }
 
 const motorcles_path = 'http://localhost:3000/api/v1/motorcycles';
 const login_path = 'http://localhost:3000/api/v1/login';
 const users_path = 'http://localhost:3000/api/v1/users/';
+const reservations_path = 'http://localhost:3000/api/v1/reservations';
 
-export {motorcles_path,login_path,users_path, request_header};
+export {motorcles_path,
+    login_path,users_path,
+    request_header,
+    reservations_path,
+    currentUser
+};
