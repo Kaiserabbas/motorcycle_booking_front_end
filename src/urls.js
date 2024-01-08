36b15f90bf254dfd5d02aa048e13ea79d6f69b72
Kaiserabbas <kayser.abbas@gmail.com>
2024-01-08 15:09:10 +0100

    update url file

--- src/urls.js
+++ src/urls.js
@@ -1,21 +1,21 @@
-const tokenObject = JSON.parse(localStorage.getItem('session_token')) || null;
-const headerArray = [];
-if (tokenObject) {
-  headerArray[0] = { headers: { Authorization: `Bearer ${tokenObject.token}`, 'Content-Type': 'application/json' } };
-}
+// const tokenObject = JSON.parse(localStorage.getItem('session_token')) || null;
+// const headerArray = [];
+// if (tokenObject) {
+//   headerArray[0] = { headers:
+//   { Authorization: `Bearer ${tokenObject.token}`, 'Content-Type': 'application/json' } };
+// }
 
-const currentUser = tokenObject?.user.name || null;
+// const currentUser = tokenObject?.user.name || null;
 
-const motorclesPath = 'http://localhost:3000/api/v1/motorcycles';
-const loginPath = 'http://localhost:3000/api/v1/login';
-const usersPath = 'http://localhost:3000/api/v1/users/';
-const reservationsPath = 'http://localhost:3000/api/v1/reservations/';
-const requestHeader = headerArray[0];
+const motorclesPath = 'http://192.168.43.117:3000/api/v1/motorcycles';
+const loginPath = 'http://192.168.43.117:3000/api/v1/login';
+const usersPath = 'http://192.168.43.117:3000/api/v1/users/';
+const reservationsPath = 'http://192.168.43.117:3000/api/v1/reservations/';
+// const requestHeader = headerArray[0];
 
 export {
   motorclesPath,
   loginPath, usersPath,
-  requestHeader,
   reservationsPath,
-  currentUser,
+  // currentUser,
 };
