d71086a9cbf91d7eaa2c5b21c4c69ddc0182b5b5
Graciano1997 <gracianomanuelhenrique@gmail.com>
2024-01-08 15:43:13 +0100

    add the modal for Ensuring the delete action from user

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,44 +1,59 @@
-import axios from 'axios';
-import { motorclesPath, requestHeader } from '../urls';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+// import axios from 'axios';
+// import { motorclesPath, requestHeader } from '../urls';
+import { deleteMotorcycles, getMotorcycles } from '../redux/motorcycleSlice';
 
 const hideModal = () => {
   document.querySelector('#deleteModal').classList.add('hideComponent');
   document.querySelector('.bodyContainer').classList.remove('hiddenScroll');
 };
 
-const deleteMotorcycle = async () => {
-  try {
-    const result = axios.delete(motorclesPath.concat('/3'), requestHeader);
-    console.log(result.data);
-    hideModal();
-    alert('apagado com sucesso!');
-  } catch (error) {
-    console.log(error);
-  }
-};
+const DeleteModal = () => {
+  const requestHeader = useSelector((state) => state.user.requestHeader);
+  const itemToDeleteId = useSelector((state) => state.motorcycle.toDeleteId);
+  const dispatch = useDispatch();
+  const [forceUpdate, setForceUpdate] = useState(false);
 
-const DeleteModal = () => (
-  <div
-    className="modalContainer hideComponent flexV"
-    id="deleteModal"
-  >
-    <div className="deleteModalContainer flexV">
-      <h3>Are you Sure you want delete this Motorcycle ?</h3>
-      <div className="confirmationButtonsContainer flexH">
-        <button id="btnYes" className="modalBtn" type="button" onClick={deleteMotorcycle}>YES</button>
-        <button
-          id="btnNo"
-          className="modalBtn"
-          type="button"
-          onClick={() => {
-            hideModal();
-          }}
-        >
-          NO
-        </button>
+  useEffect(() => {
+    dispatch(getMotorcycles(requestHeader));
+    setForceUpdate(false);
+  }, [forceUpdate]);
+  return (
+    <div
+      className="modalContainer hideComponent flexV"
+      id="deleteModal"
+    >
+      <div className="deleteModalContainer flexV">
+        <h3>Are you Sure you want delete this Motorcycle ?</h3>
+        <div className="confirmationButtonsContainer flexH">
+          <button
+            id="btnYes"
+            className="modalBtn"
+            type="button"
+            onClick={() => {
+              dispatch(deleteMotorcycles({ header: requestHeader, data: itemToDeleteId }));
+              setForceUpdate(true);
+              // dispatch(deleteMotorcycles(itemToDeleteId));
+              hideModal();
+            }}
+          >
+            YES
+          </button>
+          <button
+            id="btnNo"
+            className="modalBtn"
+            type="button"
+            onClick={() => {
+              hideModal();
+            }}
+          >
+            NO
+          </button>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default DeleteModal;
