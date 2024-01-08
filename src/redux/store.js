ecd2315c4e9558113d1d385d63ab5b0eadbd5c4a
Graciano1997 <gracianomanuelhenrique@gmail.com>
2024-01-08 15:35:58 +0100

    create and update the redux slices and actions

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import reserveReducer from './reserveSlice';
+import motorcycleReducer from './motorcycleSlice';
+import userReducer from './userSlice';
 
 const store = configureStore({
-  reducer: { reserve: reserveReducer },
+  reducer: {
+    reserve: reserveReducer,
+    motorcycle: motorcycleReducer,
+    user: userReducer,
+  },
 });
 
 export default store;
