6b0a8bf500f63a569541a8ed4d4a82b95eb632e6
Graciano1997 <gracianomanuelhenrique@gmail.com>
2024-01-08 15:45:56 +0100

    add the ItemPreview component for display the Image on create a new motorcycle form

--- src/components/ItemPreview.jsx
+++ src/components/ItemPreview.jsx
@@ -1,20 +1,44 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
+import notImageIcon from '../img/notfoundImg.png';
 
-const ItemPreview = ({ url }) => (
-  <div className="itemImagePreview flexV">
-    {url && (
-      <>
-        <h3>** Motorcycle Preview **</h3>
-        <div className="itemImagePreviewContainer">
-          <img
-            src={url}
-            alt="Could not Load the Motorcycle"
-          />
-        </div>
-      </>
-    )}
-  </div>
-);
+const ItemPreview = ({ url }) => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+  const [imageError, setImageError] = useState(false);
+  return (
+    <div className="itemImagePreview flexV">
+      {url && (
+        <>
+          <h3>** Motorcycle Preview **</h3>
+          {imageLoaded && (<h4 style={{ textAlign: 'center' }}>😍🥰</h4>)}
+
+          <div className="itemImagePreviewContainer">
+            {imageError && (
+            <img
+              src={notImageIcon}
+              alt="Notfound item"
+            />
+            )}
+            <img
+              src={url}
+              alt="motorcycle item Preview"
+              className={imageError ? 'hide' : 'previewImage'}
+              onLoad={() => {
+                setImageLoaded(true);
+                setImageError(false);
+              }}
+              onError={() => {
+                setImageError(true);
+                setImageLoaded(false);
+              }}
+            />
+          </div>
+        </>
+
+      )}
+    </div>
+  );
+};
 
 ItemPreview.propTypes = {
   url: PropTypes.string.isRequired,
