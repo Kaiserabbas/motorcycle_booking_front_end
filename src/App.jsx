f889fa6d0b8da8b723c0e9bc81098ef12ec200df
Graciano1997 <gracianomanuelhenrique@gmail.com>
2024-01-08 15:34:51 +0100

    add hamburguer in the App Component fir respond to mobile cases

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
 import './App.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import { Route, Routes } from 'react-router-dom';
@@ -12,29 +16,58 @@ import Logout from './components/Logout';
 import Signup from './components/Signup';
 import NotFound from './components/NotFound';
 import MotorcycleDetails from './components/MotorcycleDetails';
-// import NavBar from './components/NavBar';
-// import ModalLogout from './components/ModalLogout';
+import { setIconUser, setnewIconUser } from './redux/userSlice';
 
-const App = () => (
-  <>
-    <div className="app">
-      <div id="mainContainer">
-        <Routes>
-          <Route exact path="/" element={<Login />} />
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/motorcycles" element={<Motorcycles />} />
-          <Route exact path="/motorcycle" element={<MotorcycleForm />} />
-          <Route path="/motorcycle/:id" element={<MotorcycleDetails />} />
-          <Route path="/reserve" element={<Reserve />} />
-          <Route path="/reservations" element={<Reservations />} />
-          <Route path="/delete" element={<DeleteMotorcycles />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/logout" element={<Logout />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(setIconUser());
+  }, [dispatch]);
+
+  const restoreIcon = useSelector((state) => state.user.icon);
+  const currentUser = useSelector((state) => state.user.currentUser);
+
+  library.add(faXmark, faBars);
+  return (
+    <>
+      <div className="app">
+        {
+          currentUser && (
+            <div className="hamburguerContainer">
+              <FontAwesomeIcon
+                icon={restoreIcon}
+                id="hamburrguerIcon"
+                onClick={() => {
+                  if (restoreIcon === 'bars') {
+                    dispatch(setnewIconUser('xmark'));
+                    // setIcon('xmark');
+                  } else {
+                    dispatch(setnewIconUser('bars'));
+                  }
+                  document.querySelector('.navContainer').classList.toggle('controlVisibility');
+                }}
+              />
+            </div>
+          )
+        }
+        <div id="mainContainer">
+          <Routes>
+            <Route exact path="/" element={<Login />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/motorcycles" element={<Motorcycles />} />
+            <Route exact path="/motorcycle" element={<MotorcycleForm />} />
+            <Route path="/motorcycle/:id" element={<MotorcycleDetails />} />
+            <Route path="/reserve" element={<Reserve />} />
+            <Route path="/reservations" element={<Reservations />} />
+            <Route path="/delete" element={<DeleteMotorcycles />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/logout" element={<Logout />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default App;
