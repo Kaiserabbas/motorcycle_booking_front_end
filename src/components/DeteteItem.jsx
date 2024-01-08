16dbd77456071a1333a68dd36b35bed9860a10c4
Graciano1997 <gracianomanuelhenrique@gmail.com>
2024-01-08 15:41:49 +0100

    add the Motorcycle item to be deleted

--- src/components/DeteteItem.jsx
+++ src/components/DeteteItem.jsx
@@ -1,21 +1,41 @@
-const DeleteItem = () => (
-  <div className="motorcyclesItemsContainer">
-    <div className="motorcycleToDelete flexH">
-      <p>Name</p>
+// import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+// import { deleteMotorcycles, getMotorcycles } from '../redux/motorcycleSlice';
+import { setToDelete } from '../redux/motorcycleSlice';
 
-      <div className="flexV">
-        <button
-          type="button"
-          onClick={() => {
-            document.querySelector('#deleteModal').classList.remove('hideComponent');
-            document.querySelector('.bodyContainer').classList.add('hiddenScroll');
-          }}
-        >
-          Delete
-        </button>
+const DeleteItem = ({ motorcycle }) => {
+  const dispatch = useDispatch();
+  // const requestHeader = useSelector((state) => state.user.requestHeader);
+  // const [forceUpdate, setForceUpdate] = useState(false);
+
+  // useEffect(() => {
+  //   dispatch(getMotorcycles(requestHeader));
+  //   setForceUpdate(false);
+  // }, [forceUpdate]);
+  return (
+    <>
+      <div className="motorcyclesItemsContainer">
+        <div className="motorcycleToDelete flexH">
+          <h3>{motorcycle.name}</h3>
+
+          <div className="flexV">
+            <button
+              type="button"
+              onClick={() => {
+                // dispatch(deleteMotorcycles({ header: requestHeader, data: motorcycle.id }));
+                // setForceUpdate(true);
+                dispatch(setToDelete(motorcycle.id));
+                document.querySelector('#deleteModal').classList.remove('hideComponent');
+                document.querySelector('.bodyContainer').classList.add('hiddenScroll');
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-);
+    </>
+  );
+};
 
 export default DeleteItem;
