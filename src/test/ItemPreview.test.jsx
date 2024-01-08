2c8c2a97747bafcb5434663a3a9c3f31364b1221
unknown <zelalemyohannes75@gmail.com>
2024-01-08 21:45:22 +0300

    modify files

--- src/test/ItemPreview.test.jsx
+++ src/test/ItemPreview.test.jsx
@@ -2,10 +2,10 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import ItemPreview from '../components/ItemPreview'; // Update the path as needed
+import ItemPreview from '../components/ItemPreview';
 
 
 test('renders ItemPreview component without crashing when URL is missing', () => {
   render(<ItemPreview />);
-  // Add any additional checks as needed
+
 });
