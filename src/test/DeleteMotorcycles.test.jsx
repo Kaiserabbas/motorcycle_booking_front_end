a19d160c9c9be828a065b00bd20bc0a8a9aa3183
Kaiserabbas <123939543+Kaiserabbas@users.noreply.github.com>
2024-01-09 13:22:33 +0500

    Add test for delete motorcycles

---
+++ src/test/DeleteMotorcycles.test.jsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { Provider } from 'react-redux';
+import { store } from '../redux/store'; // import your store from its location
+import DeleteMotorcycles from '../components/DeleteMotorcycles';
+
+describe('DeleteMotorcycles', () => {
+    test('renders DeleteMotorcycles component without crashing', () => {
+        render(
+            <Provider store={store}>
+                <DeleteMotorcycles />
+            </Provider>
+        );
+    });
+});
