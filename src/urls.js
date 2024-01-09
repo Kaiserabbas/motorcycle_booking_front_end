347bc260b56343848b03b7965e28ec46e3590b23
Kaiserabbas <123939543+Kaiserabbas@users.noreply.github.com>
2024-01-09 18:25:03 +0500

    Update the urls for backend

--- src/urls.js
+++ src/urls.js
@@ -7,10 +7,10 @@
 
 // const currentUser = tokenObject?.user.name || null;
 
-const motorclesPath = 'http://192.168.43.117:3000/api/v1/motorcycles';
-const loginPath = 'http://192.168.43.117:3000/api/v1/login';
-const usersPath = 'http://192.168.43.117:3000/api/v1/users/';
-const reservationsPath = 'http://192.168.43.117:3000/api/v1/reservations/';
+const motorclesPath = 'https://motorcyclebooking.onrender.com/api/v1/motorcycles';
+const loginPath = 'https://motorcyclebooking.onrender.com/api/v1/login';
+const usersPath = 'https://motorcyclebooking.onrender.com/api/v1/users/';
+const reservationsPath = 'https://motorcyclebooking.onrender.com/api/v1/reservations/';
 // const requestHeader = headerArray[0];
 
 export {
