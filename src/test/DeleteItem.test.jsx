a178de826ca42a1085f9652b868b7e070f47fc75
Kaiserabbas <123939543+Kaiserabbas@users.noreply.github.com>
2024-01-11 13:05:29 +0500

    Fix the linters errors

--- src/test/DeleteItem.test.jsx
+++ src/test/DeleteItem.test.jsx
@@ -4,12 +4,11 @@ import DeleteItem from '../components/DeteteItem';
 import '@testing-library/jest-dom';
 
 describe('DeleteItem component', () => {
-
   test('renders DeleteItem component', () => {
     const motorcycle = { id: 1, name: 'Test Motorcycle' };
 
     const { getByText } = render(
-      <DeleteItem motorcycle={motorcycle} />
+      <DeleteItem motorcycle={motorcycle} />,
     );
 
     expect(getByText('Test Motorcycle')).toBeInTheDocument();
