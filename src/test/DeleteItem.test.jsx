2bbad2c1cde206acccc000110bb170039e554e7a
Graciano1997 <gracianomanuelhenrique@gmail.com>
2024-01-11 10:36:07 +0100

    update required changes

--- src/test/DeleteItem.test.jsx
+++ src/test/DeleteItem.test.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import DeleteItem from '../components/DeteteItem';
 import '@testing-library/jest-dom';
+import { Provider } from 'react-redux';
+import store from '../redux/store';
 
 describe('DeleteItem component', () => {
   test('renders DeleteItem component', () => {
     const motorcycle = { id: 1, name: 'Test Motorcycle' };
 
     const { getByText } = render(
-      <DeleteItem motorcycle={motorcycle} />,
+      <Provider store={store}>
+        <DeleteItem motorcycle={motorcycle} />,
+      </Provider>
     );
 
     expect(getByText('Test Motorcycle')).toBeInTheDocument();
--- src/test/Motorcycle.test.jsx
+++ src/test/Motorcycle.test.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Motorcycle from '../components/motorcycle';
+import { Provider } from 'react-redux';
+import store from '../redux/store';
+import { BrowserRouter } from 'react-router-dom';
 
 describe('Motorcycle component', () => {
   const motorcycleData = {
@@ -12,12 +15,23 @@ describe('Motorcycle component', () => {
   };
 
   test('renders not-found image when imageLink is not available', () => {
-    render(<Motorcycle motorcycle={motorcycleData} />);
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Motorcycle motorcycle={motorcycleData} />
+        </BrowserRouter>
+      </Provider>
+    );
     expect(screen.queryByAltText('Notfound item')).toBeFalsy();
   });
 
   test('handles image loading and error events', () => {
-    const currentScreen= render(<Motorcycle motorcycle={motorcycleData} />);
+    const currentScreen = render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Motorcycle motorcycle={motorcycleData} />
+        </BrowserRouter>
+      </Provider>);
     expect(currentScreen).toMatchSnapshot();
   });
 });
